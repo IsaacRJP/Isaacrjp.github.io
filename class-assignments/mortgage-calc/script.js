1c4c65edca0a3bcb55ed68aa628005b1d17059af
Isaac James <james.i@stu.janesville.k12.wi.us>
2025-04-25 14:35:51 -0500

    4/25

--- class-assignments/mortgage-calc/script.js
+++ class-assignments/mortgage-calc/script.js
@@ -1,4 +1,15 @@
-function calc() {
-    let output = "0"
-    
-}
+function Calc() {
+    let P = Number(document.getElementById("principle").value); // Principal
+    let r = Number(document.getElementById("irate").value)/100; // Annual interest rate
+    let n = 12;   // Number of times compounded per year
+    let t = Number(document.getElementById("year").value);   // Number of years
+    let m = Number(document.getElementById("monthpayment").value);   // Monthly payment
+  
+    let base = 1 + r / n;
+    let exponent = n * t;
+    let part1 = P * Math.pow(base, exponent);
+    let part2 = m * ((Math.pow(base, exponent) - 1) / (r / n));
+    let B = part1 - part2;
+  
+    document.getElementById("out").innerHTML = B.toFixed(2);
+  }
